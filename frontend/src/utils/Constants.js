afb55c91b1e1eb3d411b96902a56037102d34d7d
Akshay Kumar Sakinam <sakinamakshay@gmail.com>
2025-03-10 23:44:24 +0530

    baseurl updated

--- frontend/src/utils/Constants.js
+++ frontend/src/utils/Constants.js
@@ -1,3 +1,3 @@
-const BASE_URL=location.hostname==="localhost"? "http://localhost:5000":"/api";
+const BASE_URL=location.hostname==="localhost"? "http://localhost:5000":"/render";
 
 export {BASE_URL};
